1297c9f7b034f88cf6ed092acc71d31fc3b6b006
maZ <152555501+BlueTheLeaf@users.noreply.github.com>
2024-08-29 17:28:16 +0100

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,12 +1,6 @@
 @echo off
-REM Initialize a new Node.js project (creates a package.json file)
-echo Initializing Node.js project...
-npm init -y
-
-REM Install required npm packages
-echo Installing dependencies...
-npm install discord.js-selfbot-v13
-npm install prompt-sync
-
+echo Installing required Node.js modules...
+npm install discord.js-selfbot-v13 prompt-sync
 echo Installation complete.
 pause
+exit
